90d92c29aa9ccefbb326a86b0e72cba26279a99a
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-13 11:12:19 -0700

    add functionality to output desire result
    Co-authored-by: Allison Sadin <aesadin@gmail.com>

--- Program.cs
+++ Program.cs
@@ -1,9 +1,35 @@
 using System;
+
 public class Program
 {
   public static void Main()
   {
-    Console.
-
+    Console.WriteLine("Input a positive integer number:");
+    int number = int.Parse(Console.ReadLine());
+    if(number < 1) 
+    {
+      Console.WriteLine("Your input is incorrect");
+      Main();
+    }
+    Console.WriteLine("Output: ");
+    for(int i = 1; i <= number; i++)
+    {
+      if (i % 3 == 0 && i % 5 == 0) 
+      {
+        Console.WriteLine("ping-pong");
+      } 
+      else if (i % 3 == 0) 
+      {
+        Console.WriteLine("ping");
+      } 
+      else if (i % 5 == 0) 
+      {
+        Console.WriteLine("pong");
+      } 
+      else 
+      {
+        Console.WriteLine(i);
+      }
+    }
   }
 }
